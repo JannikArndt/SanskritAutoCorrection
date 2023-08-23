b2bee63c131aba5e2bfd40c900544b9617a5f93f
Jannik Arndt <jannik@jannikarndt.de>
2023-08-23 15:12:42 +0200

    First version

--- Sanskrit Auto-Correction/ContentView.swift
+++ Sanskrit Auto-Correction/ContentView.swift
@@ -10,38 +10,116 @@ import SwiftUI
 struct ContentView: View {
     let textChecker = UITextChecker()
 
-    @State var text: String = ""
+    let asanas = [
+        SanskritTerm("Adho Mukha Svanasana", "Adho Mukha Svanasana", "Downward Facing Dog"),
+        SanskritTerm("Ardha Chandrasana", "Ardha Chandrasana", "Half Moon Pose"),
+        SanskritTerm("Bakasana", "Bākāsana", "Crow Pose"),
+        SanskritTerm("Bhujangasana", "Bhujangāsana", "Cobra Pose"),
+        SanskritTerm("Chaturanga Dandasana", "Caturaṅga Daṇḍāsana", "Four-Limbed Staff Pose"),
+        SanskritTerm("Dhanurasana", "Dhanurāsana", "Bow Pose"),
+        SanskritTerm("Garudasana", "Garuḍāsana", "Eagle Pose"),
+        SanskritTerm("Gomukhasana", "Gomukhāsana", "Cow Face Pose"),
+        SanskritTerm("Halasana", "Halāsana", "Plow Pose"),
+        SanskritTerm("Hanumanasana", "Hanumānāsana", "Monkey Pose"),
+        SanskritTerm("Kapotasana", "Kapotāsana", "Pigeon Pose"),
+        SanskritTerm("Matsyasana", "Matsyāsana", "Fish Pose"),
+        SanskritTerm("Mayurasana", "Mayūrāsana", "Peacock Pose"),
+        SanskritTerm("Natarajasana", "Naṭarājāsana", "Dancer's Pose"),
+        SanskritTerm("Navasana", "Nāvāsana", "Boat Pose"),
+        SanskritTerm("Padmasana", "Padmāsana", "Lotus Pose"),
+        SanskritTerm("Parivrtta Parsvakonasana", "Parivṛtta Pārśvakonāsana", "Revolved Side Angle Pose"),
+        SanskritTerm("Paschimottanasana", "Pāścimottānāsana", "Seated Forward Bend"),
+        SanskritTerm("Salamba Sarvangasana", "Sālamba Śīrṣāsana", "Supported Shoulderstand"),
+        SanskritTerm("Salamba Sirsasana", "Sālamba Śīrṣāsana", "Supported Headstand"),
+        SanskritTerm("Savasana", "Śavāsana", "Corpse Pose"),
+        SanskritTerm("Setu Bandhasana", "Setu Bandhāsana", "Bridge Pose"),
+        SanskritTerm("Shalabhasana", "Śalabhāsana", "Locust Pose"),
+        SanskritTerm("Shavasana", "Śavāsana", "Corpse Pose"),
+        SanskritTerm("Sirsasana", "Śīrṣāsana", "Headstand"),
+        SanskritTerm("Sukhasana", "Sukhāsana", "Easy Pose"),
+        SanskritTerm("Tadasana", "Tāḍāsana", "Mountain Pose"),
+        SanskritTerm("Trikonasana", "Trikonāsana", "Triangle Pose"),
+        SanskritTerm("Ustrasana", "Uṣṭrāsana", "Camel Pose"),
+        SanskritTerm("Uttanasana", "Utthānāsana", "Standing Forward Bend"),
+        SanskritTerm("Uttitha Parsvakonasana", "Utthita Pārśvakonāsana", "Extended Side Angle Pose"),
+        SanskritTerm("Uttanasana", "Utthānāsana", "Standing Forward Bend"),
+        SanskritTerm("Vasisthasana", "Vāsiṣṭhāsana", "Side Plank Pose"),
+        SanskritTerm("Vrksasana", "Vṛkṣāsana", "Tree Pose"),
+    ]
+
+    @State var update: UUID = .init()
+    @AppStorage("UseDiacritics") var useDiacritics: Bool = false
 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Installed Keyboards:")
+        VStack(alignment: .leading, spacing: 10) {
+            Toggle("Use diacritics", isOn: $useDiacritics)
+                .padding()
+                .onChange(of: useDiacritics) { _ in
+                    update = .init()
+                }
 
-            ForEach(supportedKeyboards()) { supportedKeyboard in
-                Text("- \(supportedKeyboard.keyboard) \(supportedKeyboard.supported ? "" : "(not supported)")")
+            ScrollView {
+                HStack(alignment: .top) {
+                    Text("Asana names").font(.title)
+                    Spacer()
+                    Button("Learn all") {
+                        for word in asanas {
+                            UITextChecker.learnWord(useDiacritics ? word.withDiacritics : word.simple)
+                        }
+                        update = .init()
+                    }.buttonStyle(.borderedProminent)
+                }
+
+                LazyVStack(alignment: .leading, spacing: 10) {
+                    ForEach(asanas, id: \.id) { word in
+
+                        let term = useDiacritics ? word.withDiacritics : word.simple
+
+                        HStack(alignment: .top, spacing: 10) {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(term).bold()
+                                Text(word.english).font(.caption2)
+                            }.frame(maxWidth: 250, alignment: .leading)
+
+                            if UITextChecker.hasLearnedWord(term) {
+                                Image(systemName: "checkmark.circle")
+                                    .foregroundColor(.green)
+
+                                Button("Forget") {
+                                    UITextChecker.unlearnWord(term)
+                                    update = .init()
+                                }
+                            } else {
+                                Image(systemName: "circle.dotted")
+                                    .foregroundColor(.gray)
+
+                                Button("Learn") {
+                                    UITextChecker.learnWord(term)
+                                    update = .init()
+                                }
+                            }
+                        }
+                    }
+                }
+                .id(update)
             }
-
-            TextField("Input", text: $text)
-        }
-        .padding()
-    }
-
-    func supportedKeyboards() -> [SupportedKeyboard] {
-        let installedKeyboards = UserDefaults.standard.object(forKey: "AppleKeyboards") as? [String] ?? []
-        let keyboards = installedKeyboards.map(\.localizedLowercase).compactMap { $0.components(separatedBy: "@").first }
-        let checkerLanguages = Set(UITextChecker.availableLanguages.map(\.localizedLowercase))
-
-        return keyboards.map { keyboard in
-            let supported = checkerLanguages.contains(keyboard)
-            return SupportedKeyboard(keyboard: keyboard, supported: supported)
+            .padding()
         }
     }
 }
 
-struct SupportedKeyboard: Identifiable {
-    let keyboard: String
-    let supported: Bool
+struct SanskritTerm: Identifiable {
+    let simple: String
+    let withDiacritics: String
+    let english: String
     let id: UUID = .init()
+
+    init(_ simple: String,
+         _ withDiacritics: String,
+         _ english: String)
+    {
+        self.simple = simple
+        self.withDiacritics = withDiacritics
+        self.english = english
+    }
 }
