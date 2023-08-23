50dd5de3291f4d31c2ad904ad2e5f9a057cbffc4
Jannik Arndt <jannik@jannikarndt.de>
2023-08-23 10:27:26 +0200

    Initial Commit

---
+++ Sanskrit Auto-Correction/Sanskrit_Auto_CorrectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sanskrit_Auto_CorrectionApp.swift
+//  Sanskrit Auto-Correction
+//
+//  Created by Jannik Arndt on 23.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Sanskrit_Auto_CorrectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
