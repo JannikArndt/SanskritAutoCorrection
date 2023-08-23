6a1326e73a7ea4bbf47bf09ce33d380814dacbb0
Jannik Arndt <jannik@jannikarndt.de>
2023-08-23 11:10:50 +0200

    List languages

--- Sanskrit Auto-Correction/ContentView.swift
+++ Sanskrit Auto-Correction/ContentView.swift
@@ -8,19 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    let textChecker = UITextChecker()
+
+    @State var text: String = ""
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Installed Keyboards:")
+
+            ForEach(supportedKeyboards()) { supportedKeyboard in
+                Text("- \(supportedKeyboard.keyboard) \(supportedKeyboard.supported ? "" : "(not supported)")")
+            }
+
+            TextField("Input", text: $text)
         }
         .padding()
     }
+
+    func supportedKeyboards() -> [SupportedKeyboard] {
+        let installedKeyboards = UserDefaults.standard.object(forKey: "AppleKeyboards") as? [String] ?? []
+        let keyboards = installedKeyboards.map(\.localizedLowercase).compactMap { $0.components(separatedBy: "@").first }
+        let checkerLanguages = Set(UITextChecker.availableLanguages.map(\.localizedLowercase))
+
+        return keyboards.map { keyboard in
+            let supported = checkerLanguages.contains(keyboard)
+            return SupportedKeyboard(keyboard: keyboard, supported: supported)
+        }
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+struct SupportedKeyboard: Identifiable {
+    let keyboard: String
+    let supported: Bool
+    let id: UUID = .init()
 }
